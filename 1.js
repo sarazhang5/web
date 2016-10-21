5e62daf80449074afdce088eb3a485fed62cdd9d
sarazhang5 <1733235185@qq.com>
2016-10-21 16:21:54 +0800

    for husband website

---
+++ 1.js
@@ -0,0 +1,26 @@
+
+$(function(){
+$('.num li').mouseover(function(){
+    $('#'+this.id).addClass('act').siblings().removeClass('act');
+    $('.pho li img').fadeIn(4000).attr('src','img/'+(this.id)+'.jpg');
+})
+
+var timer;
+$('#area2').html($('#area1').html());
+function imgMarquee(){
+    if($('#scrollarea').scrollLeft()>=$('#area1').width()){
+        $('#scrollarea').scrollLeft(0)
+    }else{
+        $('#scrollarea').scrollLeft($('#scrollarea').scrollLeft()+4);
+    }
+}
+
+timer=setInterval(imgMarquee,30)
+
+$('#scrollarea').hover(function(){
+    clearInterval(timer);
+},function(){
+    timer=setInterval(imgMarquee,30)
+})
+
+})
---
+++ 4.js
@@ -0,0 +1,12 @@
+/* 
+* @Author: Marte
+* @Date:   2016-08-18 18:45:10
+* @Last Modified by:   Marte
+* @Last Modified time: 2016-08-19 14:41:27
+*/
+
+$(document).ready(function(){
+
+    
+
+});
