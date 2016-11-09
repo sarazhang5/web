ba1a7b1794f7658f5fd39756b1d9d48976bdd5f6
sarazhang5 <1733235185@qq.com>
2016-11-09 20:01:23 +0800

    code

--- doctorWeb/1.js
+++ doctorWeb/1.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2016-10-25 20:50:37
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-11-06 19:39:25
+* @Last Modified time: 2016-11-09 19:31:27
 */
 window.onload=function(){
      //弹出登陆框并加遮罩层
@@ -20,7 +20,13 @@ window.onload=function(){
      var aPsign=oSignup.getElementsByTagName('p');
      var oRembers=document.getElementById('borderSs');
      var oOks=oRembers.getElementsByTagName('span')[0];
-
+     //弹出搜索框
+     var oSearch=oHeading.getElementsByTagName('div')[2];
+     var oSearchShade=document.getElementById('search');
+     var oSpan=oSearchShade.getElementsByTagName('span')[0];
+     oSearch.onclick=function(){
+        searchShade();
+     };
      oLogin.onclick=function(){
         formMove(formLogin);
     };
@@ -70,6 +76,21 @@ window.onload=function(){
             remberBorder.style.padding="0.5px 8px";
             flag=true;
         }
+    }
+    //search的遮罩层
+    function searchShade(){
+     oSearchShade.style.display="block";
+      oSearchShade.style.backgroundColor="#01a4e4";
+      document.body.style.overflow='hidden';
+      document.body.style.height='100%';
+      document.documentElement.style.overflow='hidden'  
+    }
+    oSpan.onclick=function(){
+       oSearchShade.style.display="none"; 
+       document.body.style.overflow='visible';
+       document.documentElement.style.overflow='visible';
+       document.body.style.overflowX='hidden';
+       document.documentElement.style.overflowX='hidden';
     }
 //input获取到焦点时，清空内容.失去焦点时，内容还原。
        var aInput=document.getElementsByTagName('input');
@@ -160,6 +181,85 @@ function moveText(){
 }
 moveText();
 setInterval(moveText,2000);
+//滚动加载各个模块出现动画
+var oWelcome=document.getElementById('welcome');
+var aDiv=oWelcome.getElementsByTagName('div');
+var welcomeImg=oWelcome.getElementsByTagName('img')[0];
+var oServices=document.getElementById('services');
+var oservDiv=oServices.getElementsByTagName('div')[0];
+var aSerDiv=oservDiv.getElementsByTagName('div');
+var oTeam=document.getElementById('team');
+var ateamDiv=oTeam.getElementsByTagName('div');
+var oCapab=document.getElementById('capab');
+var ocapabDiv=oCapab.getElementsByTagName('div')[0];
+var acapabDiv=ocapabDiv.getElementsByTagName('div');
+var acapabP2=acapabDiv[1].getElementsByTagName('p')[0];
+var acapabP4=acapabDiv[3].getElementsByTagName('p')[0];
+var timercapab2=null;
+var timercapab4=null;
+window.onscroll=function(){
+    if(scrollLoad(oWelcome)){
+        welcomeImg.style.webkitTransform="rotateY(360deg)";
+        aDiv[0].style.webkitTransform="rotateY(360deg)";
+        aDiv[1].style.webkitTransform="rotateY(360deg)";
+       };
+    if(scrollLoad(oServices)){
+        aSerDiv[0].style.webkitTransform="translateY(0)";
+        aSerDiv[2].style.webkitTransform="translateY(0)";
+        aSerDiv[7].style.webkitTransform="translateY(0)";
+    };
+    if(scrollLoad(oTeam)){
+        for(var i=0;i<ateamDiv.length;i++){
+           ateamDiv[i].style.webkitTransform="scale(1)";
+        }
+        clearInterval(timercapab2);
+        clearInterval(timercapab4);
+    };
+     if(scrollLoad(oCapab)){
+         for(var i=0;i<acapabDiv.length;i++){
+           acapabDiv[i].style.webkitTransform="translateY(0)"
+        }
+
+        function add(obj,compareNumber,clearName){
+            if(obj.innerHTML==compareNumber){
+            clearInterval(clearName);
+             }else{
+            obj.innerHTML=parseInt(obj.innerHTML)+1;
+             }
+        }
+        timercapab2=setInterval(add,20,acapabP2,1700,timercapab2);
+        timercapab4=setInterval(add,20,acapabP4,2500,timercapab4);
+     }
+}
+//判断模块是否进入可视区
+function scrollLoad(model){
+   var mHeight=model.getBoundingClientRect().top;
+   var  webHeight=document.documentElement.clientHeight||document.body.clientHeight; 
+   return(mHeight<webHeight)?true:false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
